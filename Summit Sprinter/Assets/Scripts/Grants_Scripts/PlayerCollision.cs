8b4602b84bbf8731e2609117c2819f6bd2e620c2
GrantHS <6nohx1@gmail.com>
2023-09-26 11:54:24 -0700

    Made CoinSO

--- Summit Sprinter/Assets/Scripts/Grants_Scripts/PlayerCollision.cs
+++ Summit Sprinter/Assets/Scripts/Grants_Scripts/PlayerCollision.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    int numCoins = 0;
+    public PlayerDataSO playerDataSO;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Coin"))
         {
             other.gameObject.SetActive(false);
-            numCoins++;
+            playerDataSO.numCoins++;
            // Debug.Log("You have " + numCoins + " coins");
         }
 
---
+++ Summit Sprinter/Assets/Scripts/Grants_Scripts/ScriptableObjects/PlayerDataSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerDataScriptableObject", menuName = "ScriptableObjects/PlayerData")]
+public class PlayerDataSO : ScriptableObject
+{
+    public int numCoins;
+    public int totalValue;
+
+    public float playerHealth;
+    public int playerScore;
+    public int highScore;
+
+
+}
