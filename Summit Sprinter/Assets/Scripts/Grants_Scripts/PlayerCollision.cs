c3dbf5dc91dc92c39a235e3f1a670cfa7ba2c2a8
GrantHS <6nohx1@gmail.com>
2023-09-25 18:02:35 -0700

    Analyzing suspension

--- Summit Sprinter/Assets/Scripts/Grants_Scripts/PlayerCollision.cs
+++ Summit Sprinter/Assets/Scripts/Grants_Scripts/PlayerCollision.cs
@@ -11,19 +11,19 @@ public class PlayerCollision : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             numCoins++;
-            Debug.Log("You have " + numCoins + " coins");
+           // Debug.Log("You have " + numCoins + " coins");
         }
 
         if (other.gameObject.CompareTag("Gas"))
         {
             other.gameObject.SetActive(false);
-            Debug.Log("Your tank is now full");
+            //Debug.Log("Your tank is now full");
         }
 
         if (other.gameObject.CompareTag("End"))
         {
             other.gameObject.SetActive(false);
-            Debug.Log("You win!");
+            //Debug.Log("You win!");
         }
 
 
