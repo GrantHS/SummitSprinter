c926959f6d3d99214d40da49b5245c492d9225e5
GrantHS <6nohx1@gmail.com>
2023-10-10 13:29:51 -0700

    Added collision and text
    Spikey and Wheelie have collision parameters and UI text now keeps track of Coin and Skrap count

--- Summit Sprinter/Assets/Scripts/Grants_Scripts/EnemyMovement.cs
+++ Summit Sprinter/Assets/Scripts/Grants_Scripts/EnemyMovement.cs
@@ -8,63 +8,79 @@ public class EnemyMovement : MonoBehaviour
     public BadGuy badGuySO;
     private float sensorLength = 50f;
     private float yOffset = 5f;
+    private float despawnTime = 3f;
     private Rigidbody rb;
+    private GameObject player;
+    private bool attacked = false; //did the enemy attack already?
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
     }
 
     private void Update()
     {
-        RaycastHit hit;
-        Physics.Raycast(transform.position, transform.up, out hit, sensorLength);
-        Debug.DrawRay(transform.position, transform.up, Color.red);
-
-        if(hit.collider != null)
+        switch (badGuySO.enemyType)
         {
-            if (hit.collider.gameObject.GetComponent<PlayerMovement>())
-            {
-                //Debug.Log("Player Detected");
-                GameObject player = hit.collider.gameObject;
-                player.GetComponent<PlayerCollision>().TakeDamage(badGuySO);
-                switch (badGuySO.enemyType)
+            case Enemies.Spikey:
+                //Debug.Log("Spikey position: " + this.transform.localPosition.y + "| Player position: " + player.transform.localPosition.y);
+                RaycastHit hit;
+                Physics.Raycast(transform.position, transform.up, out hit, sensorLength);
+                Debug.DrawRay(transform.position, transform.up, Color.red);
+
+                if (hit.collider != null)
                 {
-                    case Enemies.Spikey:
-                        //Debug.Log("Spikey position: " + this.transform.localPosition.y + "| Player position: " + player.transform.localPosition.y);
-                        if (this.transform.localPosition.y - yOffset <= player.transform.localPosition.y)
-                        {
-                            //Debug.Log("adding force");
-                            transform.Translate(Vector3.up * badGuySO.speed * Time.deltaTime); 
-                            
-                        }
-                        else
-                        {
-                            //Debug.Log("not adding force");
-                            GetComponent<BoxCollider>().isTrigger = true;
-                        }
-                        break;
-                    case Enemies.Wheelie:
-                        break;
-                    case Enemies.Rocketee:
-                        break;
-                    default:
-                        break;
+                    if (hit.collider.gameObject.GetComponent<PlayerMovement>())
+                    {
+                        //Debug.Log("Player Detected");
+                        player = hit.collider.gameObject;
+                        //player.GetComponent<PlayerCollision>().TakeDamage(badGuySO);
+
+                    }
+                    if (this.transform.localPosition.y - yOffset <= player.transform.localPosition.y)
+                    {
+                        //Debug.Log("adding force");
+                        transform.Translate(Vector3.up * badGuySO.speed * Time.deltaTime);
+
+                    }
+                    else
+                    {
+                        //Debug.Log("not adding force");
+                        GetComponent<BoxCollider>().isTrigger = true;
+                    }
                 }
-            }
+                break;
+            case Enemies.Wheelie:
+                break;
+            case Enemies.Rocketee:
+                break;
+            default:
+                break;
         }
+        
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.gameObject.GetComponent<PlayerMovement>())
         {
+            player = collision.collider.gameObject;
+
             switch (badGuySO.enemyType)
             {
                 case Enemies.Spikey:
+
                     GetComponent<BoxCollider>().isTrigger = true;
+                    if (!attacked)
+                    {
+                        player.GetComponent<PlayerMovement>().gasMeter.GasDrain += badGuySO.damage;
+                        attacked = true;
+                    }                   
                     break;
                 case Enemies.Wheelie:
+                    StartCoroutine(Ragdoll(player));
                     break;
                 case Enemies.Rocketee:
                     break;
@@ -72,6 +88,16 @@ public class EnemyMovement : MonoBehaviour
                     break;
             }
         }
+    }
+
+    private IEnumerator Ragdoll(GameObject player)
+    {
+        rb.isKinematic = false;
+        rb.AddForce(Vector3.left * badGuySO.speed, ForceMode.Impulse);
+        player.GetComponent<PlayerMovement>().gasMeter.currentValue -= badGuySO.damage;
+        yield return new WaitForSeconds(despawnTime);
+        gameObject.SetActive(false);
+
     }
 
 
